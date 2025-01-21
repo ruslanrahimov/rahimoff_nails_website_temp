c064dfc8be5101418eb43dfc8b27465792a9d7c8
Ruslan Rahimov <rahimoff.tr@gmail.com>
2025-01-21 12:29:11 +0300

    add new config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import webfontDownload from 'vite-plugin-webfont-dl'
+import { VitePluginRadar } from 'vite-plugin-radar'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
     plugins: [
         webfontDownload(),
+        VitePluginRadar({
+            // Google Analytics tag injection
+            analytics: {
+                enableDev: true,
+                id: 'G-FC18865LEX',
+            },
+        })
     ],
 })
